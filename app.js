57b37eea4387b1df93b7d24150dd460a12360f52
MahaJY <158135608+MahaJY@users.noreply.github.com>
2024-02-22 20:20:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use('/api', employeeroute);
 
-const PORT = process.env.PORT || 7000;
+const PORT =  7000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

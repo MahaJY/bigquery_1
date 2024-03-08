7056dfdcab8efc6584c718435f8433344d7756b6
MahaJY <mahalakshmijaisankar66@gmail.com>
2024-03-08 12:21:31 +0530

    init commit

--- controllers/control.js
+++ controllers/control.js
@@ -7,6 +7,61 @@ const oAuth2Client = new google.auth.OAuth2(
     process.env.REDIRECT_URI
 );
 oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+const bytext  = async (req, res) => {
+    const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+    const text = req.params.text
+    try {
+        const fivedaysago = moment().subtract(30,'days').format('YYYY-MM-DD')
+        const customQuery=`in:inbox  after:${fivedaysago} ${text}`;
+        const response = await gmail.users.messages.list({
+            userId: 'me',
+            q:customQuery
+        });
+        const messages = response.data.messages;
+        if (messages && messages.length > 0) {
+            const getEmailPromises = messages.map(async (message) => {
+                const email = await gmail.users.messages.get({
+                    userId: 'me',
+                    id: message.id,
+                });
+                const { id, snippet, labelIds, payload } = email.data;
+                const subject = payload.headers.find(header => header.name === 'Subject').value;
+                const sender = payload.headers.find(header => header.name === 'From').value;
+                const date = payload.headers.find(header => header.name === 'Date').value;
+                let body = '';
+    if (payload.parts) {
+        body = payload.parts.reduce((acc, part) => {
+            if (part.body && part.body.data) {
+                acc += Buffer.from(part.body.data, 'base64').toString('utf-8');
+            }
+            return acc;
+        }, '');
+    } else if (payload.body && payload.body.data) {
+        body = Buffer.from(payload.body.data, 'base64').toString('utf-8');
+    }
+                return {
+                    id,
+                    snippet,
+                    labelIds,
+                    subject,
+                    sender,
+                    date,
+                    body
+                };
+            });
+        
+            const emails = await Promise.all(getEmailPromises);
+            console.log('emails found:',+emails.length)
+            res.json(emails);
+        }
+         else {
+            res.status(404).json({ message: 'No emails found.' }); ;
+        }
+    } catch (error) {
+        console.error('The API returned an error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
 
 const bysubject  = async (req, res) => {
     const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
@@ -117,4 +172,4 @@ const bymail  = async (req, res) => {
     }
 };
 
-module.exports = { bymail,bysubject };
+module.exports = { bymail,bysubject,bytext };
